9346356be12c784ed2d169252d874bc404d3fff9
justmirshod <98507846+justmirshod@users.noreply.github.com>
2023-02-16 14:04:37 +0500

    `initial changes`

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import mirshod from "./assets/mirshod.jpg";
+import reactLogo from "./assets/react.svg?url";
+
+function App() {
+  console.log(mirshod);
+  return <div className="text-2xl text-red-800">Hello world</div>;
+}
+
+export default App;
